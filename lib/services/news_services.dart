885b6c11189788759e16f00c602cf8364d0cc5fa
wilzzadex19 <a.waliyuddin@kabayan.id>
2021-04-20 22:43:38 +0700

    ss

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,1 +1,22 @@
 part of 'services.dart';
+
+class NewsServices {
+  static Future<ApiReturnValue<List<News>>> getNews({http.Client client}) async {
+    client ??=  http.Client();
+
+    String url = baseURL2 + 'api/mobile/news';
+    var response = await client.get(url,headers:{'Content-Type' : 'apllication/json'});
+
+
+    if(response.statusCode != 200){
+      return ApiReturnValue(message:'Gagal mengambil data');
+    }
+
+    var data = aConvert.jsonDecode(response.body);
+    List<News> news = (data['response'] as Iterable)
+        .map((e) => News.fromMap(e))
+        .toList();
+
+    return ApiReturnValue(value : news);
+  }
+}
