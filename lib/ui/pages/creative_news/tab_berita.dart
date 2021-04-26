f329c45173130d0813652696e925bc681bc9b119
wilzzadex <a.waliyuddin@kabayan.id>
2021-04-26 08:31:03 +0700

    ss

--- lib/ui/pages/creative_news/tab_berita.dart
+++ lib/ui/pages/creative_news/tab_berita.dart
@@ -1,7 +1,13 @@
 part of '../pages.dart';
 
-class TabBerita extends StatelessWidget {
-  final List<int> beritaItems = [1, 2, 3, 4, 5];
+class TabBerita extends StatefulWidget {
+  @override
+  _TabBeritaState createState() => _TabBeritaState();
+}
+
+class _TabBeritaState extends State<TabBerita> {
+ final List<int> beritaItems = [1, 2, 3, 4, 5];
+  int _current = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -42,90 +48,125 @@ class TabBerita extends StatelessWidget {
           if (newsState is NewsLoaded) {
             ApiReturnValue<List<News>> news = newsState.news;
             List<News> newsValue = news.value.sublist(0, 5);
-            return Container(
-              height: 125,
-              margin: EdgeInsets.symmetric(horizontal: defaultMargin),
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: newsValue.length,
-                itemBuilder: (BuildContext ctx, int index) => InkWell(
-                  onTap: () {
-                    // context.bloc<NewsBloc>().add(FetchNewsDetail(newsValue[index].id));
-                    Get.to(NewsDetail(newsValue[index]));
-                  },
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 122,
-                        width: 140,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: CachedNetworkImage(
-                            fit: BoxFit.cover,
-                            imageUrl: newsValue[index].imgEvent[0],
-                            progressIndicatorBuilder:
-                                (context, url, downloadProgress) => Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      gradient: LinearGradient(
-                                        colors: [
-                                          Colors.black.withOpacity(0.61),
-                                          Colors.blueAccent.withOpacity(0),
-                                        ],
-                                        begin: Alignment.bottomCenter,
-                                        end: Alignment.topCenter,
-                                      ),
-                                    ),
-                                    child: SpinKitFadingCircle(
-                                        color: backgroundColorGrey, size: 50)),
-                            errorWidget: (context, url, error) =>
-                                Icon(Icons.error),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                            left: 15,
-                            right: (index == newsValue.length - 1) ? 0 : 15),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+            return CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 16 / 9,
+                viewportFraction: 1,
+                initialPage: 0,
+                height: 150,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                    print("${_current}");
+                  });
+                },
+              ),
+              items: newsValue
+                  .map((item) => InkWell(
+                        onTap: () {
+                          Get.to(NewsDetail(item));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Stack(
                             children: [
                               Container(
-                                width:
-                                    (MediaQuery.of(context).size.width) - 192,
-                                child: Text(
-                                  newsValue[index].title,
-                                  maxLines: 2,
-                                  style: titleStyle,
+                                height: 140,
+                                width: (MediaQuery.of(context).size.width) - 40,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: 122,
+                                      width: 140,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: CachedNetworkImage(
+                                          fit: BoxFit.cover,
+                                          imageUrl: item.imgEvent[0],
+                                          progressIndicatorBuilder: (context,
+                                                  url, downloadProgress) =>
+                                              Container(
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            15),
+                                                    gradient: LinearGradient(
+                                                      colors: [
+                                                        Colors.black
+                                                            .withOpacity(0.61),
+                                                        Colors.blueAccent
+                                                            .withOpacity(0),
+                                                      ],
+                                                      begin: Alignment
+                                                          .bottomCenter,
+                                                      end: Alignment.topCenter,
+                                                    ),
+                                                  ),
+                                                  child: SpinKitFadingCircle(
+                                                      color:
+                                                          backgroundColorGrey,
+                                                      size: 50)),
+                                          errorWidget: (context, url, error) =>
+                                              Icon(Icons.error),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.only(
+                                          left: 15, right: defaultMargin),
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              width: (MediaQuery.of(context)
+                                                      .size
+                                                      .width) -
+                                                  215,
+                                              child: Text(
+                                                item.title,
+                                                style: titleStyle,
+                                                maxLines: 2,
+                                              ),
+                                            ),
+                                           
+                                            SizedBox(height: 10),
+                                            Container(
+                                              width: (MediaQuery.of(context)
+                                                      .size
+                                                      .width) -
+                                                  220,
+                                              child: Text(
+                                                (item.description == null)
+                                                    ? '-'
+                                                    : parse(item.description)
+                                                            .documentElement
+                                                            .text
+                                                            .substring(0, 90) +
+                                                        '...',
+                                                style: normalFontStyle.copyWith(
+                                                    color: "333333".toColor(),
+                                                    fontSize: 12),
+                                                textAlign: TextAlign.left,
+                                              ),
+                                            ),
+                                          ]),
+                                    ),
+                                  ],
                                 ),
                               ),
-                              SizedBox(height: 10),
-                              Container(
-                                width:
-                                    (MediaQuery.of(context).size.width) - 192,
-                                child: Container(
-                                  child: Text(
-                                    parse(newsValue[index].description)
-                                            .documentElement
-                                            .text
-                                            .substring(0, 130) +
-                                        '...',
-                                    style: normalFontStyle.copyWith(
-                                        color: "333333".toColor()),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                ),
-                              ),
-                            ]),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+                            ],
+                          ),
+                        ),
+                      ))
+                  .toList(),
             );
           } else if (newsState is NewsFilterLoaded) {
             return Container(
@@ -173,7 +214,9 @@ class TabBerita extends StatelessWidget {
               ),
             );
           } else {
-            return Center(child: SpinKitFadingCircle(color: backgroundColorGrey,size: 50));
+            return Center(
+                child:
+                    SpinKitFadingCircle(color: backgroundColorGrey, size: 50));
           }
         }),
         SizedBox(
@@ -184,90 +227,50 @@ class TabBerita extends StatelessWidget {
         BlocBuilder<NewsBloc, NewsState>(builder: (_, newsState) {
           if (newsState is NewsLoaded) {
             ApiReturnValue<List<News>> news = newsState.news;
-            List<News> newsValue = news.value.sublist(0, 5);
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: newsValue
-                  .map(
-                    (val) => Container(
-                      height: 10,
-                      width: 10,
-                      margin: EdgeInsets.symmetric(horizontal: 3),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                            color: Color(0xFFFFFFFF),
-                            offset: Offset(0.0, -1.0),
-                            blurRadius: 2.0,
-                          ),
-                          BoxShadow(
-                            color: Color(0xFFCACACA),
-                            offset: Offset(0.0, 1.0),
-                            blurRadius: 2.0,
-                          ),
-                          BoxShadow(
-                            color: Color.fromRGBO(51, 51, 51, 0.16),
-                            offset: Offset(0, 0.0),
-                            blurRadius: 0.0,
-                          ),
-                        ],
-                        gradient: LinearGradient(
-                          begin: Alignment(0.0, 0.0),
-                          end: Alignment(0.996, 0.092),
-                          colors: [
-                            "DFDFDF".toColor(),
-                            "FEFEFE".toColor(),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                  .toList(),
-            );
-          } else if (newsState is NewsFilterLoaded) {
-            ApiReturnValue<List<News>> news = newsState.news;
             List<News> newsValue = news.value;
+            
             return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: newsValue
-                  .map(
-                    (val) => Container(
-                      height: 10,
-                      width: 10,
-                      margin: EdgeInsets.symmetric(horizontal: 3),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                            color: Color(0xFFFFFFFF),
-                            offset: Offset(0.0, -1.0),
-                            blurRadius: 2.0,
-                          ),
-                          BoxShadow(
-                            color: Color(0xFFCACACA),
-                            offset: Offset(0.0, 1.0),
-                            blurRadius: 2.0,
-                          ),
-                          BoxShadow(
-                            color: Color.fromRGBO(51, 51, 51, 0.16),
-                            offset: Offset(0, 0.0),
-                            blurRadius: 0.0,
-                          ),
-                        ],
-                        gradient: LinearGradient(
-                          begin: Alignment(0.0, 0.0),
-                          end: Alignment(0.996, 0.092),
-                          colors: [
-                            "DFDFDF".toColor(),
-                            "FEFEFE".toColor(),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                  .toList(),
-            );
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: newsValue
+                            .map(
+                              (item) => Container(
+                                height: 10,
+                                width: 10,
+                                margin: EdgeInsets.symmetric(horizontal: 3),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: <BoxShadow>[
+                                    BoxShadow(
+                                      color: Color(0xFFFFFFFF),
+                                      offset: Offset(0.0, -1.0),
+                                      blurRadius: 2.0,
+                                    ),
+                                    BoxShadow(
+                                      color: Color(0xFFCACACA),
+                                      offset: Offset(0.0, 1.0),
+                                      blurRadius: 2.0,
+                                    ),
+                                    BoxShadow(
+                                      color: Color.fromRGBO(51, 51, 51, 0.16),
+                                      offset: Offset(0, 0.0),
+                                      blurRadius: 0.0,
+                                    ),
+                                  ],
+                                  gradient: LinearGradient(
+                                    begin: Alignment(0.0, 0.0),
+                                    end: Alignment(0.996, 0.092),
+                                    colors: [
+                                      // "DFDFDF".toColor(),
+                                      // (_current == newsValue.indexOf(item))
+                                      //     ? mainColorRed
+                                      //     : "FEFEFE".toColor(),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      );
           } else {
             return SizedBox();
           }
@@ -503,7 +506,9 @@ class TabBerita extends StatelessWidget {
                   .toList(),
             );
           } else {
-            return Center(child: SpinKitFadingCircle(color: backgroundColorGrey, size: 50));
+            return Center(
+                child:
+                    SpinKitFadingCircle(color: backgroundColorGrey, size: 50));
           }
         }),
         SizedBox(height: 100),
